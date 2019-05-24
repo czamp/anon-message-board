b736f5efe0fd0c5007e124291a626561ce57efe0
czamp <clay.zamperini@gmail.com>
2019-05-24 13:06:09 -0700

    Create TimeStamp component

---
+++ client/src/components/TimeStamp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TimeStamp = ({time}) => (
+  <time dateTime={new Date(time)}>
+    {new Date(time).toLocaleString("en-US")}
+  </time>
+);
+
+export default TimeStamp;
