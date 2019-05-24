d83798e7f06cf3aa31c2f7441f67970cecc641f9
czamp <clay.zamperini@gmail.com>
2019-05-24 13:41:46 -0700

    Better formatting for SingleThread

--- client/src/components/SingleThread.js
+++ client/src/components/SingleThread.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Container, Header, Segment } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 import Thread from './Thread';
 import BreadcrumbNav from './BreadcrumbNav';
 
 
 const SingleThread = (props) => (
-  <React.Fragment>
+  <Container text>
     <BreadcrumbNav board={props.match.params.board} thread_id={props.match.params.id} />
     <Thread board={props.match.params.board} id={props.match.params.id}/>
-  </React.Fragment>
+    <Segment vertical>
+      <Header as="h4">
+        <Link to={`/b/${props.match.params.board}`}>Click here to return to /b/{props.match.params.board}/</Link>
+      </Header>
+    </Segment>
+  </Container>
 )
 
 export default SingleThread
