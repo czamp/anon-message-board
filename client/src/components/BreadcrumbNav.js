e7cce8f5942b89f089dd677460fc0505614f8208
czamp <clay.zamperini@gmail.com>
2019-05-24 11:05:15 -0700

    Create BreadcrumbNav component

---
+++ client/src/components/BreadcrumbNav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Breadcrumb } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const BreadcrumbNav = props => (
+  <Breadcrumb>
+    <Breadcrumb.Section>
+      <Link to="/">BOARDS</Link>
+    </Breadcrumb.Section>
+    {props.board && (
+      <React.Fragment>
+        <Breadcrumb.Divider />
+        <Breadcrumb.Section>
+          <Link to={`/b/${props.board}/`}>
+            {props.board.toUpperCase()}
+          </Link>
+        </Breadcrumb.Section>
+      </React.Fragment>
+    )}
+    {props.thread_id && (
+      <React.Fragment>
+        <Breadcrumb.Divider />
+        <Breadcrumb.Section>
+          <Link to={`/b/${props.board}/thread/${props.thread_id}`}>
+            {props.thread_id}
+          </Link>
+        </Breadcrumb.Section>
+      </React.Fragment>
+    )}
+  </Breadcrumb>
+);
+
+export default BreadcrumbNav;
